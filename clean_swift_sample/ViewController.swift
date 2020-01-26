18d7ca3d187cdabcb0503872dd4107f83a0d8a86
olive <oliveoil211@gmail.com>
2020-01-26 13:10:21 +0900

    Initial Commit

---
+++ clean_swift_sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clean_swift_sample
+//
+//  Created by RyoOkamto on 2020/01/26.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
