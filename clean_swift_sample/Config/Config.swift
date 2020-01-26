5b723f097369650a8e12075d0a8d9134f67530cf
olive <oliveoil0211@gmail.com>
2020-01-26 19:08:49 +0900

    Create a config file

---
+++ clean_swift_sample/Config/Config.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  clean_swift_sample
+//
+//  Created by RyoOkamto on 2020/01/26.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import Foundation
+
+class Config {
+    static let API_KEY = "77d3161216cca86d9b842db3854500c8"
+}
