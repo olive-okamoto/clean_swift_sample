1fdc448cfa867d3607c77cea13a2f31ea4c7576d
olive <oliveoil0211@gmail.com>
2020-02-08 14:08:03 +0900

    Implement repository

--- clean_swift_sample/Domain/Repository/CityWeatherRepository.swift
+++ clean_swift_sample/Domain/Repository/CityWeatherRepository.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 oliveapps. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+
+// MARK: - Interface
+public protocol CityWeatherRepository {
+    func getWeather(by city: String, country: String) -> Observable<WeatherModel>
+}
+
+// MARK: - Inplementation
+struct CityWeatherRepositoryImpl: CityWeatherRepository {
+    
+    private let dataStore: CityWeatherDataStore
+    
+    public init(dataStore: CityWeatherDataStore) {
+        self.dataStore = dataStore
+    }
+    
+    func getWeather(by city: String, country: String) -> Observable<WeatherModel> {
+        return dataStore.getWeather(by: city, country: country)
+    }
+    
+}
