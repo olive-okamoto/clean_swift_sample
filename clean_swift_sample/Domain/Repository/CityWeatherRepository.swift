fc90197424c708b516d96ad6f4c12e824ffa6b97
olive <oliveoil0211@gmail.com>
2020-02-08 13:50:38 +0900

    Create a new repository

---
+++ clean_swift_sample/Domain/Repository/CityWeatherRepository.swift
@@ -0,0 +1,9 @@
+//
+//  CityWeatherRepository.swift
+//  clean_swift_sample
+//
+//  Created by RyoOkamto on 2020/02/08.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import Foundation
