2f2241019270f9aa49a24d0e1cfdd213f420f095
olive_okamoto <oliveoil0211@gmail.com>
2020-02-08 18:21:50 +0900

    Create a table view cell and its xib

---
+++ clean_swift_sample/Presentation/View/Cell/CitySelectTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CitySelectTableViewCell.swift
+//  clean_swift_sample
+//
+//  Created by RyoOkamto on 2020/02/08.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import UIKit
+
+class CitySelectTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
