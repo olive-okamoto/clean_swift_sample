dd76d8df5ec443219fecb70849a330d3fb0f61e9
olive_okamoto <oliveoil0211@gmail.com>
2020-02-09 20:31:32 +0900

    Create a builder object

---
+++ clean_swift_sample/Application/Builder/CityListBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  CityListBuilder.swift
+//  clean_swift_sample
+//
+//  Created by RyoOkamto on 2020/02/09.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import UIKit
+
+struct CityListBuilder {
+    func build() -> UIViewController {
+        
+    }
+}
