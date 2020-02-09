c21d1f6f5f1f2d609411748604e0bb0d105c0f96
olive_okamoto <oliveoil0211@gmail.com>
2020-02-09 19:56:35 +0900

    Implement use cases function from a presenter and save a data

--- clean_swift_sample/Presentation/Presenter/CityWeatherPresenter.swift
+++ clean_swift_sample/Presentation/Presenter/CityWeatherPresenter.swift
@@ -7,21 +7,59 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol CityWeatherPresenter {
+    func getCityList()
     func selectCity(_ index: Int)
 }
 
 class CityWeatherPresenterImpl: CityWeatherPresenter {
     
-    private let useCase: CityWeatherUseCase
+    let useCase: CityWeatherUseCase
+    let cityUseCase: CityListUseCase
+    var cityListModel: CityListModel?
     
-    public required init(useCase: CityWeatherUseCase) {
+    private let disposeBag = DisposeBag()
+    
+    public required init(useCase: CityWeatherUseCase, cityUseCase: CityListUseCase) {
         self.useCase = useCase
+        self.cityUseCase = cityUseCase
+    }
+    
+    func getCityList() {
+        cityUseCase.getCityList()
+            .subscribe(
+                onNext: { [weak self] cityList in
+                    self?.loadCityListModel(cityList: cityList)
+                }, onError: nil, onCompleted: nil, onDisposed: nil)
+            .disposed(by: disposeBag)
     }
     
     func selectCity(_ index: Int) {
-        //useCase.getWeather(by: )
+        guard let cityListModel = cityListModel else { return }
+        
+        let selectedCity: CityModel = cityListModel.cities[index]
+        
+        useCase.getWeather(by: selectedCity)
+            .subscribe(
+                onNext: { [weak self] weather in
+                    // TODO: Display weather in a different view
+                }, onError: { [weak self] error in
+                    // TODO: Error handling
+                }, onCompleted: nil, onDisposed: nil)
+            .disposed(by: disposeBag)
+    }
+    
+}
+
+// MARK: - Private
+extension CityWeatherPresenterImpl {
+    
+    fileprivate func loadCityListModel(cityList: CityListModel) {
+        DispatchQueue.main.async {
+            self.cityListModel = cityList
+        }
     }
     
 }
