6d33b4f93f635af5b5b8938d16299d1d701ac076
olive_okamoto <oliveoil0211@gmail.com>
2020-02-09 19:16:13 +0900

    Create a use case for gettinag city list

---
+++ clean_swift_sample/Domain/UseCase/CityListUseCase.swift
@@ -0,0 +1,27 @@
+//
+//  CityListUseCase.swift
+//  clean_swift_sample
+//
+//  Created by RyoOkamto on 2020/02/09.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol CityListUseCase {
+    func getCityList() -> Observable<[CityModel]>
+}
+
+struct CityListUseCaseImpl: CityListUseCase {
+    private let repository: CityListRepository
+    
+    init(repository: CityListRepository) {
+        self.repository = repository
+    }
+    
+    func getCityList() -> Observable<[CityModel]> {
+        return repository.getCityList()
+    }
+}
+
